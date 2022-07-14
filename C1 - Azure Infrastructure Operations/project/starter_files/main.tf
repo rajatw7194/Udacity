e3b2a7615a1d4f2eb73d4bb95f65e3ec5e9c3393
rajatw7194 <75608128+rajatw7194@users.noreply.github.com>
2022-07-15 04:47:05 +0530

    Create main.tf

---
+++ C1 - Azure Infrastructure Operations/project/starter_files/main.tf
@@ -0,0 +1,120 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "main" {
+  name     = "${var.prefix}-rg"
+  location = var.location
+}
+
+resource "azurerm_virtual_network" "main" {
+  name                = "${var.prefix}-network"
+  address_space       = ["10.0.0.0/22"]
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+}
+
+resource "azurerm_subnet" "internal" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_network_interface" "main" {
+  name                = "${var.prefix}-nic"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.internal.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_network_security_group" "main" {
+  name                = "${var.prefix}-nsg"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  security_rule {
+    name                       = "Allow_VM_Subnets"
+    priority                   = 101
+    protocol                   = "*"
+    source_address_prefix      = "10.0.2.0/24"
+    destination_address_prefix = "10.0.2.0/24"
+    direction                  = "Inbound"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    access                     = "Allow"
+  }
+
+  security_rule {
+    name                       = "Deny_Internet"
+    priority                   = 100
+    protocol                   = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+    direction                  = "Inbound"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    access                     = "Deny"
+  }
+
+ resource "azurerm_public_ip" "main" {
+  name                = "${var.prefix}-public_ip"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  allocation_method   = "Dynamic"
+} 
+
+ resource "azurerm_load_balancer" "main" {
+  name                = "${var.prefix}-lb"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  frontend_ip_configuration {
+    name                 = "${var.prefix}-frontend_ip"
+    public_ip_address_id = azurerm_public_ip.main.id
+  }
+}
+
+resource "azurerm_load_balancer_backend_address_pool" "main" {
+  resource_group_name = azurerm_resource_group.main.name
+  loadbalancer_id     = azurerm_lb.main.id
+  name                = "${var.prefix}-backend_address_ip"
+}
+  
+resource "azurerm_linux_virtual_machine" "main" {
+  name                            = "${var.prefix}-vm"
+  resource_group_name             = azurerm_resource_group.main.name
+  location                        = azurerm_resource_group.main.location
+  size                            = "Standard_D2s_v3"
+  admin_username                  = "${var.username}"
+  admin_password                  = "${var.password}"
+  disable_password_authentication = false
+  network_interface_ids = [
+    azurerm_network_interface.main.id,
+  ]
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+
+  resource "azurerm_managed_disk" "main" {
+  name                 = "${var.prefix}-md"
+  location             = azurerm_resource_group.main.location
+  resource_group_name  = azurerm_resource_group.main.name
+  storage_account_type = "Standard_LRS"
+  create_option        = "Empty"
+  disk_size_gb         = "1"
+}
+  os_disk {
+    storage_account_type = "Standard_LRS"
+    caching              = "ReadWrite"
+  }
+}
